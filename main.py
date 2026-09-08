8c4bce18d61522920fe61e869f2a5d15c6f20247
Kenneth Yang <kjy@is88.com>
2026-09-08 09:28:50 +0200

    Add CORS, add -p flag (#8)

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from argparse import ArgumentParser
+
 from uvicorn import run
 
 from ephys_link.server import app
 
+# Parse CLI.
+parser = ArgumentParser()
+parser.add_argument(
+    "-p", "--port", type=int, default=8000, help="Server port (default: 8000)"
+)
+args = parser.parse_args()
+
+# Launch.
 if __name__ == "__main__":
-    run(app, host="0.0.0.0", port=8000)
+    run(app, host="0.0.0.0", port=args.port)
