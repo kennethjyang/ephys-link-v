d053f31f19fdac862b93e0ef9ce3dea30088c653
Kenneth Yang <kjy@is88.com>
2026-09-12 12:04:50 +0200

    Add set and end task helpers

--- ephys_link/tasks.py
+++ ephys_link/tasks.py
@@ -1,3 +1,5 @@
+from time import time
+
 from ephys_link.models import TaskState
 
 # Map of Task ID (UUID) -> Task State.
@@ -7,6 +9,7 @@ tasks: dict[str, TaskState] = {}
 def remove_manipulator(task_id: str, make: str, manipulator_id: str) -> bool:
     """Remove a manipulator from a task.
 
+    If the task ID doesn't exist, nothing will happen.
     If the manipulator isn't in the task, nothing will change.
 
     Args:
@@ -17,6 +20,10 @@ def remove_manipulator(task_id: str, make: str, manipulator_id: str) -> bool:
     Returns:
         True if there are no more manipulators left in the task.
     """
+    # Exit if the task doesn't exist.
+    if task_id not in tasks:
+        return False
+
     target_task = tasks[task_id]
 
     updated_manipulators = target_task.manipulators - {(make, manipulator_id)}
@@ -26,3 +33,40 @@ def remove_manipulator(task_id: str, make: str, manipulator_id: str) -> bool:
     )
 
     return len(updated_manipulators) == 0
+
+
+def set_message(task_id: str, message: str | None = None) -> None:
+    """Set a message for a task.
+
+    If the task ID doesn't exist, nothing will happen.
+
+    Args:
+        task_id: ID of the task to set the message to.
+        message: Message to add to the task (None would remove it).
+    """
+    # Exit if the task doesn't exist.
+    if task_id not in tasks:
+        return
+
+    target_task = tasks[task_id]
+    tasks[task_id] = target_task.model_copy(update={"message": message})
+
+
+def end_task(task_id: str, message: str | None = None):
+    """Marks a task as inactive and adds an optional message.
+
+    If the task ID doesn't exist, nothing will happen.
+
+    Args:
+        task_id: ID of the task to mark as inactive.
+        message: Optional message to add to the task.
+    """
+    # Exit if the task doesn't exist.
+    if task_id not in tasks:
+        return
+
+    target_task = tasks[task_id]
+
+    tasks[task_id] = target_task.model_copy(
+        update={"time_ended": time(), "message": message}
+    )
