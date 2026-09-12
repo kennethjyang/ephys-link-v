68cc9b8a777c1a132cee8ce4674bedd932228aa1
Kenneth Yang <kjy@is88.com>
2026-09-12 13:50:41 +0200

    Make manipulator finding synchronous

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 
 from uvicorn import run
 
+from ephys_link.manipulators import find_manipulators
 from ephys_link.server import app
 
 # Parse CLI.
@@ -13,4 +14,5 @@ args = parser.parse_args()
 
 # Launch.
 if __name__ == "__main__":
+    find_manipulators()
     run(app, host="0.0.0.0", port=args.port)
