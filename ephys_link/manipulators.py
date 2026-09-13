6487cdf6e40fbb8d4653878c6deb1089533f4cc8
Kenneth Yang <kjy@is88.com>
2026-09-13 15:23:42 +0200

    Populate with Sensapex manipulator

--- ephys_link/manipulators.py
+++ ephys_link/manipulators.py
@@ -1,4 +1,7 @@
+from sensapex import UMP
+
 from ephys_link.base_binding import BaseBinding
+from ephys_link.bindings.sensapex_binding import SensapexBinding
 
 # Mapping of detected manipulators at startup.
 # Make -> ID -> Binding.
@@ -10,5 +13,16 @@ def find_manipulators() -> dict[str, dict[str, BaseBinding]]:
     """Search across binding interfaces for manipulators and return them."""
     global manipulators
 
+    # Reset.
     manipulators = {}
+
+    # Sensapex.
+    ump = UMP.get_ump()
+    found_manipulators = ump.list_devices()
+
+    for manipulator_id in found_manipulators:
+        manipulators["sensapex"][str(manipulator_id)] = SensapexBinding(
+            str(manipulator_id)
+        )
+
     return manipulators
