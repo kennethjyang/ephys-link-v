7197f8d0b9e2395bbf3f6d3ed76b23751cf5e2eb
Kenneth Yang <microbob@is88.com>
2026-09-18 23:13:35 +0200

    Try-catch-finally goto pos

--- ephys_link/base_binding.py
+++ ephys_link/base_binding.py
@@ -31,7 +31,7 @@ class BaseBinding(ABC):
     ) -> None:
         """Sets the position of the manipulator while updating the task.
 
-        Once the task is complete, implementer _must_ end the task.
+        Implementer _must_ end the task.
 
         Args:
             position: List of absolute positions to write to the manipulator in mm.
